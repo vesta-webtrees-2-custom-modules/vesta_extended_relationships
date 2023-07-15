5876c45eb20f84667a37475922227d0ecc4ffe79
Richard Cissée <ric@richard-cissee.de>
2023-07-15 17:45:04 +0200

    Address #106

--- autoload.php
+++ autoload.php
@@ -5,5 +5,6 @@ use Composer\Autoload\ClassLoader;
 $loader = new ClassLoader();
 $loader->addPsr4('Cissee\\Webtrees\\Module\\ExtendedRelationships\\', __DIR__);
 $loader->addPsr4('Cissee\\WebtreesExt\\', __DIR__ . "/patchedWebtrees");
+$loader->addPsr4('Cissee\\WebtreesExt\\Services\\', __DIR__ . "/patchedWebtrees/Services");
 $loader->addPsr4('Cissee\\WebtreesExt\\Functions\\', __DIR__ . "/patchedWebtrees/Functions");
 $loader->register();
